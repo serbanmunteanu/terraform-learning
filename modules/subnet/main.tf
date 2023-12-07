3af4f6ce839416841f5d3f757d82212e2aecd168
Serban Munteanu <33052622+serbanmunteanu@users.noreply.github.com>
2023-12-07 09:45:00 +0200

    Patch 1 (#1)
    * Add files via upload
    
    * Create main.tf
    
    * Create main.tf
    
    * Update main.tf
    
    * Add files via upload
    
    * Add files via upload

---
+++ modules/subnet/main.tf
@@ -0,0 +1,28 @@
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = var.vpc_id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.availability_zone
+  tags = {
+    Name: "${var.env_prefix}-subnet-1"
+  }
+}
+
+resource "aws_internet_gateway" "my-app-igw" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name: "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = var.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-app-igw.id
+  }
+
+  tags = {
+    Name: "${var.env_prefix}-rtb"
+  }
+}
